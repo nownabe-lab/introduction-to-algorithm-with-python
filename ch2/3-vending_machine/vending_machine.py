a00eefbc1aedec8240e5fe9e176e63c916bac043
nownabe <nownabe@gmail.com>
2020-09-19 21:03:36 +0900

    2.3 vending_machine4.py

--- ch2/3-vending_machine/vending_machine.py
+++ ch2/3-vending_machine/vending_machine.py
@@ -1,6 +1,19 @@
+import sys
+
 insert_price = input('insert: ')
+if not insert_price.isdecimal():
+    print('Please input an integer.')
+    sys.exit()
+
 product_price = input('product: ')
+if not product_price.isdecimal():
+    print('Please input an integer.')
+    sys.exit()
+
 change = int(insert_price) - int(product_price)
+if change < 0:
+    print('Insufficient charges')
+    sys.exit()
 
 coins = [5000, 1000, 500, 100, 50, 10, 5, 1]
 
